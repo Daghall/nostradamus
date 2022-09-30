df10bf76497845ed4a91711f23147c1c05a696a0
Markus Daghall <markus@daghall.se>
2022-09-30 15:29:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,113 @@
+# pythia
+
+> _Pythia  was the name of the high priestess of the Temple of Apollo at Delphi.
+> She specifically served as its oracle and was known as the Oracle of Delphi._
+
+Deterministically mock values from `Math.random`.
+
+Values can be given as a single number, an array or a function returning a value. Optionally stop mocking after the values have been used.
+
+Values are verified to be of type `number` and in the range `[0,1]`, as this is what `Math.random` returns.
+An error is thrown when unexpected values are detected.
+
+## Installation
+
+```bash
+$ npm install --save-dev the-pythia
+```
+
+## Usage
+
+```javascript
+const pythia = require("the-pythia");
+
+pythia.predict(0.5);
+
+console.log(Math.random()); // 0.5
+```
+
+## API
+
+### `predict(prediction [, options])`
+
+#### Parameters
+
+##### `prediction`
+
+The value(s) to return.
+
+Accepts the following types:
+
+| Type       | `Math.random()` returns |
+| ---------- | ----------------------- |
+| `number`   | the given number |
+| `array`    | each number in the array per subsequent call |
+| `function` | the return value of the function, until it returns something `falsy` |
+
+---
+##### `options`
+
+An options object that only has one settings: `repeat`.
+Setting this to `false` will reset `Math.random` when the value(s) have been exhausted. Has no effect on `function` predictions, since it resets as soon as the function returns something falsy.
+
+### `isPredicting()`
+
+Returns a `boolean` reflecting if _pythia_ is predicting random values.
+
+### `forget()`
+
+Reset `Math.random` to its original functionality.
+
+### `reset()`
+
+Alias for `forget()`.
+
+## Examples
+
+```javascript
+const pythia = require("the-pythia");
+
+// Type: number
+pythia.predict(0.1982);
+
+Math.random(); // 0.1982
+Math.random(); // 0.1982
+
+
+// Type: number, no repeat
+pythia.predict(0.42, { repeat: false });
+
+Math.random(); // 0.42
+Math.random(); // Psuedo-random value between 0 and 1
+
+
+// Type: array
+pythia.predict([0.31, 0.41, 0.52]);
+
+Math.random(); // 0.31
+Math.random(); // 0.41
+Math.random(); // 0.52
+Math.random(); // 0.31
+
+
+// Type: array, no repeat
+pythia.predict([0.31, 0.41, 0.52], { repeat: false });
+
+Math.random(); // 0.31
+Math.random(); // 0.41
+Math.random(); // 0.52
+Math.random(); // Psuedo-random value between 0 and 1
+
+
+// Type: function
+let fakeRandomCalls = 0;
+pythia.predict(() => {
+  if (fakeRandomCalls++ > 2) return false;
+  return 0.8219
+});
+
+Math.random(); // 0.8219
+Math.random(); // 0.8219
+Math.random(); // 0.8219
+Math.random(); // Psuedo-random value between 0 and 1
+```
---
+++ test/pythia-test.js
@@ -0,0 +1,127 @@
+"use strict";
+
+const expect = require("chai").expect;
+const pythia = require("../lib/pythia.js");
+const { PythiaError } = require("../lib/errors.js");
+
+describe("pythia", () => {
+  beforeEach(() => {
+    expect(pythia.isPredicting()).to.equal(false);
+  });
+
+  afterEach(() => {
+    pythia.forget();
+    expect(pythia.isPredicting()).to.equal(false);
+  });
+
+  it("returns a number – indefinitely", () => {
+    pythia.predict(0.123456);
+
+    expect(Math.random()).to.equal(0.123456);
+    expect(pythia.isPredicting()).to.equal(true);
+    expect(Math.random()).to.equal(0.123456);
+    expect(Math.random()).to.equal(0.123456);
+    expect(pythia.isPredicting()).to.equal(true);
+  });
+
+  it("returns a number – once", () => {
+    pythia.predict(0.123456, {repeat: false});
+
+    expect(pythia.isPredicting()).to.equal(true);
+    expect(Math.random()).to.equal(0.123456);
+    expect(pythia.isPredicting()).to.equal(false);
+    expect(Math.random()).to.not.equal(0.123456);
+  });
+
+  it("returns each number from an array – indefinitely", () => {
+    const predictions = [0, 0.1, 0.2, 0.3];
+    pythia.predict(predictions);
+
+    runTwice(() => {
+      expect(pythia.isPredicting()).to.equal(true);
+      for (const prediction of predictions) {
+        expect(Math.random()).to.equal(prediction);
+      }
+    });
+  });
+
+  it("returns each number from an array – once", () => {
+    const predictions = [0, 0.1, 0.2, 0.3];
+    pythia.predict(predictions, {repeat: false});
+
+    expect(pythia.isPredicting()).to.equal(true);
+    for (const prediction of predictions) {
+      expect(Math.random()).to.equal(prediction);
+    }
+
+    expect(pythia.isPredicting()).to.equal(false);
+
+    const random = Math.random();
+    expect(random).to.be.within(0, 1);
+    expect(random).to.not.equal(predictions[0]);
+  });
+
+  it("returns a number from a function – indefinitely", () => {
+    let number = 0;
+    pythia.predict(() => {
+      return ++number / 10;
+    });
+
+    for (let i = 1; i < 7; ++i) {
+      expect(pythia.isPredicting()).to.equal(true);
+      expect(Math.random()).to.equal(i / 10);
+    }
+  });
+
+  it("returns a number from a function, until falsy", () => {
+    const predictions = [0.4, 0.2];
+    pythia.predict(() => {
+      return predictions.shift();
+    });
+
+    expect(pythia.isPredicting()).to.equal(true);
+    expect(Math.random()).to.equal(0.4);
+
+    expect(pythia.isPredicting()).to.equal(true);
+    expect(Math.random()).to.equal(0.2);
+
+    expect(pythia.isPredicting()).to.equal(true);
+    expect(Math.random()).to.be.within(0, 1);
+    expect(pythia.isPredicting()).to.equal(false);
+  });
+
+  it("asserts realistic random value", () => {
+    pythia.predict(12);
+    expect(Math.random).to.throw(PythiaError, "Random value must be less than or equal to one");
+
+    pythia.predict(-1);
+    expect(Math.random).to.throw(PythiaError, "Random value must be greater than or equal to zero");
+  });
+
+  it("throws when no arguments are given", () => {
+    const fn = () => pythia.predict();
+    expect(fn).to.throw(PythiaError, "Unknown prediction type");
+  });
+
+  it("throws when prediction is a string", () => {
+    const fn = () => pythia.predict("random string");
+    expect(fn).to.throw(PythiaError, "Unknown prediction type");
+  });
+
+  it("throws when prediction array contains an object", () => {
+    pythia.predict([0.4711, {}]);
+    expect(Math.random()).to.be.within(0, 1);
+    expect(Math.random).to.throw(PythiaError, "Random value must be a number");
+  });
+
+  it("throws when prediction is an object", () => {
+    const fn = () => pythia.predict({});
+    expect(fn).to.throw();
+  });
+});
+
+function runTwice(fn) {
+  Array(2).fill().forEach(() => {
+    fn();
+  });
+}
